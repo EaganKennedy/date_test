f3486b6ff31e40a93a975e820cb5399295ead251
Eagan <eegykennedy@gmail.com>
2025-09-21 22:37:59 -0500

    Test Framework complete

--- date_test/date_test.cpp
+++ date_test/date_test.cpp
@@ -3,3 +3,40 @@
 
 using namespace util;
 
+TEST(DefaultCtor, InitAndBaseGetter) {
+	// test constructor = 1/1/1970 MM/DD/YY using getters
+}
+
+TEST(Setters, SettingAndGetting) {
+	// test setting with the same three dates
+}
+
+TEST(ValueCtor, InitWorks) {
+	// test using three dates, one on the low, mid and high ends
+}
+
+TEST(Props, CorrectRead) {
+	// test Feb, Aug, Nov, Sun, Wed, Fri
+}
+
+TEST(Print, Prints) {
+	// Prints
+}
+
+
+TEST(Advance, BasicAdvance) {
+	// no input, five input, negative
+}
+
+TEST(Advance, DateOrder) {
+	// Date works in each order ad changes across multiple
+}
+
+TEST(Advance, Seperator) {
+	// Seperator influences all
+}
+
+
+TEST(Now, WorkingStatic) {
+	// Static works
+}
