d9ba52fa7a2265125959ccd0b2bbed6aa038b00b
Eagan <eegykennedy@gmail.com>
2025-09-21 21:45:20 -0500

    date.hpp initialized

--- date/date.cpp
+++ date/date.cpp
@@ -1,1 +1,7 @@
-﻿#include "date.hpp"
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "date.hpp"
+#include <ctime>
+#include <sstream>
+
+using namespace util;
+
--- date/date.hpp
+++ date/date.hpp
@@ -1,5 +1,56 @@
 ﻿#pragma once
+#include <ctime>
+#include <string>
 
 namespace util {
+	class Date {
+	public:
+		struct Invalid {
+			int day;
+			int month;
+			int year;
+		};
 
+		enum class Order {
+			MonthDayYear,
+			DayMonthYear,
+			YearMonthDay
+		};
+
+		Date(); // set date to 1/1/1970 MM/DD/YY
+		Date(int d, int m, int y);
+
+		void day(int d);
+		int day();
+
+		void month(int m);
+		int month();
+
+		void year(int y);
+		int year();
+
+		std::string dayName();
+		std::string monthName();
+
+		void advance(int move = 1);
+
+		void print(std::ostream&);
+
+		static Date now();
+
+		static Order order; // Order::MonthDayYear
+		static std::string separator; // '/'
+
+
+
+	private:
+
+		int formatDay(); //Exists for convention, doesn't do anything
+		int formatMonth(); // add 1
+		int formatYear(); // add 1900
+
+
+		time_t cTime;
+		tm oTime;
+	};
 }
